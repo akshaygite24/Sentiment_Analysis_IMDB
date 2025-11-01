26de38dc2f3f2e6ce00ae6a8a82e13fab7207472
akshaygite24 <akshaygite2004@gmail.com>
2025-11-01 22:20:07 +0530

    fixed mapping issue

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -12,7 +12,7 @@ stop_words = set(stopwords.words("english"))
 
 def clean_code(text):
     text = re.sub(r"<.*?>", " ", text)
-    text = re.sub(r"[^a-zA-Z]", " ", text)
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
     text = text.lower()
     words = text.split()
     words = [lemmatizer.lemmatize(w) for w in words if w not in stop_words]
